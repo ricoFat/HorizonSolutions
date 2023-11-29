db3acde81b00af97051655a0791efc776068a974
ricoFat <accram2005@hotmail.com>
2023-11-29 15:48:58 -0500

    changement dans contact- retrait de services et partenaires

--- mail.php
+++ mail.php
@@ -37,28 +37,4 @@ if (isset($_POST['email']) && !empty($_POST['email']))
         header("Location: contact.html");
     }
 }
-
-/* 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $tel = $_POST['tel'];
-    $message = $_POST['message'];
-
-    $mailheader = "From:".$name."<".$email.">\r\n";
-
-    $recipient = "info@horizonsolutionsarl.com";
-
-    $mail_sent = mail($recipient, $tel, $message, $mailheader);
-
-    if ($mail_sent) 
-    {
-        // Si le courriel est envoyé; on retourne à la page d'accueil
-        header("Location: index.html");
-    } 
-    else 
-    {
-        // Sinon on envoie un message d'erreur et on reste sur la page contact
-        die("Error sending the message");
-        header("Location: contact.html");
-    } */
 ?>
